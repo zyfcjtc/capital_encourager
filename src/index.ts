61fc961998bf3574e8c099ac876d114d7790f41f
Yafan Zhang <yafanzhang@Yafans-MacBook-Pro-2.local>
2022-10-26 18:10:37 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import { window } from 'vscode'
+import { window, commands, workspace } from 'vscode'
 
 export function activate() {
-  window.showInformationMessage('Hello')
+  let disposable = commands.registerCommand('developer_encourager.showhello', () => {
+    window.showInformationMessage('Hello')
+  });
+  
+  let counter = 5
+  let intervalId = setInterval(() => {
+    counter -= 1
+    console.log(counter)
+    if (counter === 0) {
+      window.showInformationMessage('Need some help?')
+    }
+  }, 1000)
+  workspace.onDidChangeTextDocument(async ()=>{
+    counter = 5
+    window.showInformationMessage('text changed')
+  })
+  
+  
 }
 
 export function deactivate() {
