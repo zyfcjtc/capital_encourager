91bc609b8f19f4056be6b2681b7f71724ac8c724
Yafan Zhang <yafanzhang@Yafans-MacBook-Pro-2.local>
2022-10-27 13:23:17 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { window, commands, workspace } from 'vscode'
+import { window, commands, workspace, ExtensionContext } from 'vscode'
 
-export function activate() {
-  let disposable = commands.registerCommand('developer_encourager.showhello', () => {
-    window.showInformationMessage('Hello')
+export function activate(context: ExtensionContext) {
+  let getEncouragement = commands.registerCommand('developer_encourager.encouragement', () => {
+    window.showInformationMessage('encourager enabled')
+    let counter = 10
+    let intervalId = setInterval(() => {
+      counter -= 1
+      console.log(counter)
+      if (counter === 0) {
+        window.showInformationMessage('Need some help?')
+      }
+    }, 1000)
+    workspace.onDidChangeTextDocument(async ()=>{
+      counter = 10
+      window.showInformationMessage('text changed')
+    })
   });
-  
-  let counter = 5
-  let intervalId = setInterval(() => {
-    counter -= 1
-    console.log(counter)
-    if (counter === 0) {
-      window.showInformationMessage('Need some help?')
-    }
-  }, 1000)
-  workspace.onDidChangeTextDocument(async ()=>{
-    counter = 5
-    window.showInformationMessage('text changed')
-  })
-  
-  
+  context.subscriptions.push(getEncouragement)
 }
 
 export function deactivate() {
